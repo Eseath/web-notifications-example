beb2c5bedb3f7229f2ec4f87aefa19286f38898f
Ruslan <ruslan.a94@yandex.com>
2019-10-29 20:20:13 +0300

    Update the mongoose package

--- app/database.js
+++ app/database.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://default:secret@database:27017/main', {
-    useMongoClient: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 mongoose.connection.on('error', () => {
     console.log('MongoDB connection error. Please make sure MongoDB is running.');
